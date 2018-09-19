a0ec58dcda34d0f7e8bf49b5c3058335f7348f91
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-09-19 18:33:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function takeANumber(katzDeliLine, name) {
 //add Ada (or whatever name is passed in) to the end of the line
 katzDeliLine.push(name)
-var number = katzDeliLine.length
+var number = katzDeliLine.length 
 
 //return a string welcoming the person and telling them what number in line they are
  return `Welcome, ${name}. You are number ${number} in line.`
@@ -20,12 +20,14 @@ function nowServing(katzDeliLine) {
 }
 function currentLine(katzDeliLine) {
   var currentLine = []
-  if (katzDeliLine.length === 0) {
-  return "The line is currently empty."
-} else {
-  var name = katzDeliLine.shift()
-  return `The line is currently: 1. ${name}, 2. Jane, 3. Ann`
+ if (katzDeliLine.length === 0) {
+ return "The line is currently empty."
+} 
   
-}
+  
+  for (let i = 0; i < katzDeliLine.length; i++) {
+    katzDeliLine.push(` `+[i+1]+`. `  + katzDeliLine[i])
+   }
+ return `The line is currently: + katzDeliLine`
 
 }  
